0c7294b516c00f7c2bf43087a66caa2324d26ee6
Tishyo Saha <tishyo77@gmail.com>
2023-12-11 10:08:22 +0530

    Fourth Update

--- client/src/LandingPage.jsx
+++ client/src/LandingPage.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './LandingPage.css';
+import './LandingPage.css'; 
+import Logo from './Images/SSLogo.png'; // Replace with the actual path to your logo image
 
 const LandingPage = () => {
   return (
-    <div className="menu">
-      <Link to="/signup" className="sign-up-button">Sign Up</Link>
-      <Link to="/login" className="log-in-button">Log In</Link>
+    <div className="landing-page">
+      <div className="navbar">
+        <div className="logo">
+          <img src={Logo} alt="Your Logo" />
+        </div>
+        <div className="login-btn">Log In</div>
+      </div>
+
+      <div className="content">
+        <h1>Embrace Tranquility: <br></br> Nurturing Mental Wellness</h1>
+        <br></br>
+        <p>Welcome to SerenitySync, your dedicated companion on the journey to mental well-being. We understand the importance of maintaining a healthy mind in today's fast-paced world. SerenitySync is not just a mental health monitoring website; it's your sanctuary for tranquility. <br></br> <br></br> Immerse yourself in our curated collection of guided meditations, explore insightful self-help books, and engage with empowering podcasts, all designed to uplift and support your mental health. Our user-friendly platform seamlessly integrates mindfulness practices into your daily routine, helping you achieve a sense of balance and serenity. Join the SerenitySync community and embark on a path of self-discovery and mental wellness. Your peace of mind is just a click away.</p>
+        <button className="signup-btn">Sign Up</button>
+      </div>
     </div>
   );
 };
